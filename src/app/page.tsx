4a284543f4651390ef932298c235a403d6a2d0fd
abriki <abriki@hubspot.com>
2025-03-08 00:28:22 +0100

    svg + css

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { SingleScreenLanding } from "../components/landing/SingleScreenLanding";
+
+export default function Home() {
+  return (
+    <main>
+      <SingleScreenLanding />
+    </main>
+  );
+}
